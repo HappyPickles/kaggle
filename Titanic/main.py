52dc888d8b67ae59f8dd23f416f23b0ff3a28a96
GuoXu <786385048@qq.com>
2023-11-08 19:43:18 +0800

    Use English for annotations.

--- Titanic/main.py
+++ Titanic/main.py
@@ -3,19 +3,17 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-from sklearn import tree
-from sklearn import model_selection
-from sklearn import ensemble
-from sklearn import neural_network
-from sklearn import svm
+from sklearn import *
 
 
-# 数据读取
+# Read Datasets
 TrainDf = pd.read_csv('train.csv')
 TestDf = pd.read_csv('test.csv')
 AnsDf = pd.read_csv('gender_submission.csv')
 
-# 数据预处理
+# Preprocess
+# TODO: We need to do more preprocessing.
+
 for Df in [TrainDf, TestDf]:
 
     for column in ['Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare']:
@@ -26,7 +24,7 @@ for Df in [TrainDf, TestDf]:
         IQR = Q2 - Q1
         N = 1.5
         Df.loc[((Df[column] < Q1 - N * IQR) | (Df[column] > Q2 + N * IQR)), column] = np.nan
-        # 超过1.5倍IQR作为异常值删去
+        # Remove IQR exceeding 1.5 times as outlier.
 
     Df['Sex'].replace(['male', 'female'], [0, 1], inplace=True)
     Df['Embarked'].replace(['S', 'C', 'Q'], [0, 1, 2], inplace=True)
@@ -36,37 +34,36 @@ for Df in [TrainDf, TestDf]:
     Df.loc[Df['Age'] > 12, 'Age'] = 0
 
 
-# 特征选择
+# Features Selection
 # corr = TrainDf.corr()
 # print(corr['Survived'])
 FeaturesColumns = ['Pclass', 'Sex', 'Age', 'SibSp']
 Target = 'Survived'
 # print(TrainDf[FeaturesColumns].describe())
 
-# 数据集分割
-#
-# best = [0, 0, 0]
-# for i, model in enumerate([tree.DecisionTreeClassifier(), ensemble.RandomForestClassifier(),
-#                            neural_network.MLPClassifier(), svm.SVC(), xgb.XGBClassifier()]):
-#
-#     for size in [0.1 * i for i in range(1, 10)]:
-#         S_TrainDf, ValDF = model_selection.train_test_split(TrainDf, test_size=size, random_state=True)
-#         model.fit(S_TrainDf[FeaturesColumns], S_TrainDf[Target])
-#         val_score = model.score(ValDF[FeaturesColumns], ValDF[Target])
-#         test_score = model.score(TestDf[FeaturesColumns], AnsDf[Target])
-#         if val_score > best[2]:
-#             best[0] = i
-#             best[1] = size
-#             best[2] = val_score
-#         print('当前size为 %.2f ' % size)
-#         print('当前model index为 %s' % i)
-#         print(val_score)
-#         print(test_score)
-#         print('--' * 20)
-# print(best)
+# Model Selection
+best = [0, 0, 0]
+for i, model in enumerate([tree.DecisionTreeClassifier(), ensemble.RandomForestClassifier(),
+                           neural_network.MLPClassifier(), svm.SVC(), xgb.XGBClassifier(),
+                           ensemble.GradientBoostingClassifier(), ensemble.AdaBoostClassifier()]):
+    for size in [0.1 * i for i in range(1, 10)]:
+        S_TrainDf, ValDF = model_selection.train_test_split(TrainDf, test_size=size, random_state=True)
+        model.fit(S_TrainDf[FeaturesColumns], S_TrainDf[Target])
+        val_score = model.score(ValDF[FeaturesColumns], ValDF[Target])
+        test_score = model.score(TestDf[FeaturesColumns], AnsDf[Target])
+        if val_score > best[2]:
+            best[0] = i
+            best[1] = size
+            best[2] = val_score
+        print('size %.2f ' % size)
+        print('model index %s' % i)
+        print(val_score)
+        print(test_score)
+        print('--' * 20)
+print(best)
 
 
-
+# Predict
 TrainDf, ValDF = model_selection.train_test_split(TrainDf, test_size=0.5, random_state=True)
 sub_model = tree.DecisionTreeClassifier()
 sub_model.fit(TrainDf[FeaturesColumns], TrainDf[Target])
