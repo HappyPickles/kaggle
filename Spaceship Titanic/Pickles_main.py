91a55f4f0e34a66b9a635a7d494f2627fff29fe5
HappyPickles <786385048@qq.com>
2023-12-06 18:58:27 +0800

    update

--- Spaceship Titanic/Pickles_main.py
+++ Spaceship Titanic/Pickles_main.py
@@ -8,19 +8,21 @@ from utils.preprocess import *
 train_df = pd.read_csv(r'train.csv')
 test_df = pd.read_csv(r'test.csv')
 print(train_df.corr()['Transported'])
-
-
-def consume(df):
-    return df['RoomService'] + df['FoodCourt'] + df['ShoppingMall'] + df['Spa'] + df['VRDeck']
+consume_list = ['TotalConsume', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 
 
 def preprocess(df: pd.DataFrame):
-    df.fillna(method='pad', inplace=True)
     df = dummy_process(df, ['HomePlanet', 'Destination'])
-    df.loc[:, 'TotalConsume'] = df.apply(consume, axis=1)
-    df.loc[df['TotalConsume'] == 0 & df['CryoSleep'].isnull(), 'CryoSleep'] = 'TRUE'
-#    df.loc[df['CryoSleep'] == 'TRUE' & df['TotalConsume'].isnull(), 'TotalConsume', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck'] = 0
+    df.replace({True: 1, False: 0}, inplace=True)
+    df.loc[:, 'TotalConsume'] = df['RoomService'] + df['FoodCourt'] + df['ShoppingMall'] + df['Spa'] + df['VRDeck']
+    df.loc[df['TotalConsume'] == 0 & df['CryoSleep'].isnull(), 'CryoSleep'] = 1
+    df.loc[df['TotalConsume'] != 0 & df['CryoSleep'].isnull(), 'CryoSleep'] = 0
+    df.loc[(df['CryoSleep'] == 1) & (df['TotalConsume'].isnull()), consume_list] = 0
+    df.loc[df['CryoSleep'] == 0, consume_list].fillna(df.loc[df['CryoSleep'], consume_list].median())
 
+
+
+    df.fillna(method='pad', inplace=True)
 
     return df
 
@@ -29,3 +31,5 @@ train_df = preprocess(train_df)
 test_df = preprocess(test_df)
 
 print(train_df.info())
+print(train_df['Transported'])
+print(train_df.corr()['Transported'])
