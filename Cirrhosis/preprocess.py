8a788d49db6bdb17c3d716cb253ac96abd360832
HappyPickles <786385048@qq.com>
2023-12-12 15:14:54 +0800

    update Cirrhosis preprocess

--- Cirrhosis/preprocess.py
+++ Cirrhosis/preprocess.py
@@ -8,10 +8,27 @@ train_df = pd.read_csv(r'train.csv')
 test_df = pd.read_csv(r'test.csv')
 
 
-def preprocess(df: pd.DataFrame):
+norm_range = {'Bilirubin': [0.3, 1.3], 'Cholesterol': [[0, 200], [200, 240]],
+              'Albumin': [[0, 3.5], [3.5, 5.5]], 'Copper': [15, 30], 'Alk_Phos': [45, 130],
+              'SGOT': [8, 50], 'Platelets': [100, 300], 'Prothrombin': [11, 13]}
 
 
+def preprocess(df: pd.DataFrame, test=False):
 
+    df.replace({'Y': 1, 'N': 0}, inplace=True)
+    df = dummy_process(df, ['Drug', 'Sex', 'Stage'])
+    df = in_range_process(df, norm_range, drop=False)
+    df['is_elder'] = 0
+    df.loc[df['Age'] >= 365*60, 'is_elder'] = 1
+    df.replace({True: 1, False: 0}, inplace=True)
 
+    if not test:
+        df = dummy_process(df, ['Status'])
 
     return df
+
+
+pre_train_df = preprocess(train_df)
+pre_test_df = preprocess(test_df, test=True)
+pre_train_df.to_csv('pre_train.csv')
+pre_test_df.to_csv('pre_test.csv')
